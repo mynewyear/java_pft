3e456ec3436cedbf4d277a1230f1312ddf23037a
mynewyear <natalia.iakovleva@hotmail.com>
2016-07-31 19:30:42 -0700

    lesson 02 module 9
    ApplicationManager added

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupCreationTest extends TestBase {
+
+
+    @Test
+    public void testGroupCreation() {
+        app.goToGroupPage();
+        app.initGroupCreation();
+        app.fillGoupForm(new GroupData("test1", "test2", "test3"));
+        app.submitGoupCreation();
+        app.returnToGroupPage();
+    }
+
+}
