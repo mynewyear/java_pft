232999b26ba0ee99153fdc702cb23d70f4876bc0
mynewyear <natalia.iakovleva@hotmail.com>
2016-09-09 16:58:36 -0700

    lesson 8 module 04

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTest.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.mantis.tests;
+
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by IEUser on 9/9/2016.
+ */
+public class RegistrationTest extends TestBase {
+
+    @Test
+    public void testRegistration() {
+                app.registration().start("user1", "user1@localhost.localdomain");
+    }
+}
