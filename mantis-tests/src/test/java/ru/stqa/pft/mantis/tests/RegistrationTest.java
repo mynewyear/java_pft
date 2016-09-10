70feaa517032fd3418d32c26aff3ffbe4cdc0601
mynewyear <natalia.iakovleva@hotmail.com>
2016-09-09 23:52:27 -0700

    lesson 8 module 05

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTest.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTest.java
@@ -10,6 +10,6 @@ public class RegistrationTest extends TestBase {
 
     @Test
     public void testRegistration() {
-                app.registration().start("user1", "user1@localhost.localdomain");
+                app.registration().start(" user1", "user1@localhost.localdomain");
     }
 }
